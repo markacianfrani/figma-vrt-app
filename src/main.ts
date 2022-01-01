7bb08ae1a8502119f6331dc3ec767a19bd5ef203
mark cianfrani <markacianf@gmail.com>
2022-01-01 16:33:55 -0500

    feat: add analytics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import store from './store'
-import router from './routes'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueGtag from "vue-gtag";
+import "./index.css";
+import store from "./store";
+import router from "./routes";
 
-const app = createApp(App)
-app.use(store).use(router).mount('#app')
-// app.use(router)
-// app.mount('#app')
+const app = createApp(App);
+app.use(store).use(router);
+
+app.use(
+  VueGtag,
+  {
+    pageTrackerExcludedRoutes: ["callback"],
+    config: { id: "G-82THN7JJ97" },
+  },
+  router
+);
+app.mount("#app");
